fb3d5cda989354a5f45f8f9ef6e48ec00a6be0e0
Gunalan183 <gunalana18@gmail.com>
2026-09-02 20:29:02 +0530

    Change the CSS style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -37,7 +37,7 @@ export default function HomePage() {
       <ServicesSection />
       
       <WhyChooseSection />
-      <HowItWorksSection />
+      
       <OwnerContactSection />
     </Layout>
   );
