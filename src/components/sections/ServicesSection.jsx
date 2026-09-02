178da0e2b012125fdfd4c7ecb6f2bdc1178298eb
Gunalan183 <gunalana18@gmail.com>
2026-09-02 16:45:42 +0530

    Change the CSS style

--- src/components/sections/ServicesSection.jsx
+++ src/components/sections/ServicesSection.jsx
@@ -240,7 +240,7 @@ export default function ServicesSection() {
         </div>
 
         {/* Cards grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
           {SERVICES.map((service) => (
             <ServiceCard key={service.id} service={service} onDetail={openModal} />
           ))}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -35,7 +35,7 @@ export default function HomePage() {
       <MachineryShowcase />
       <ServiceHighlights />
       <ServicesSection />
-      <MachineryShowcase />
+      
       <WhyChooseSection />
       <HowItWorksSection />
       <OwnerContactSection />
